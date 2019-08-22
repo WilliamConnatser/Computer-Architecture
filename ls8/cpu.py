b7091f0a36cc50f3a65344e318533770f5613903
WilliamConnatser <WilliamConnatser@users.noreply.github.com>
2019-08-22 16:57:44 -0500

    day2 mvp finished

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -14,31 +14,26 @@ class CPU:
 
     def load(self):
         """Load a program into memory."""
-
+        #Get filepath as a parameter
         try:
-            program = sys.argv[1]
+            filepath = sys.argv[1]
         except:
             print("Provide the filepath to the program you want to run...")
             sys.exit(1)
 
+        # Read the source code
+        source_code = open(filepath, 'r')
+        line = source_code.readline()
         address = 0
-
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
-
+        while line:
+            # Filter out empty lines and lines which only contain comments
+            if len(line.strip()) != 0 and not line.strip().startswith("#"):
+                #Filter out in-line comments
+                if "#" in line:
+                    line = line[:line.index("#")]
+                self.ram_write(address,line)
+                address += 1
+            line = source_code.readline()
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -53,7 +48,12 @@ class CPU:
     def ram_read(self, mar):
         # mar = Memory Address Register (MAR) and the Memory Data Register (MDR).
         # The MAR contains the address that is being read or written to.
-        return self.ram[mar]
+
+        # Convert binary string to int
+        try:
+            return int(self.ram[mar],2)
+        except:
+            return 0
 
     def ram_write(self, mar, mdr):
         # mdr = Memory Data Register (MDR)
